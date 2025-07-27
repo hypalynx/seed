efb90b20ac6e42cfc52f6566f2e3bc6b84d1bd12
Mike Bruce <mike@hypalynx.com>
2025-07-27 02:36:30 +0100

    simple page switch animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { h, Fragment } from "preact";
-import { Link, Route, Switch, useLocation } from "wouter-preact";
-import { useState, useEffect } from "preact/hooks";
+import { Link, Route, Router, Switch, useLocation } from "wouter-preact";
 import { Counter } from "./page/Counter";
 import { Index } from "./page/Index";
 import { DesignPreview } from "./page/DesignPreview";
@@ -8,36 +7,23 @@ import { Nav } from "./component/Nav";
 
 export const App = () => {
   const [location] = useLocation();
-  const [displayLocation, setDisplayLocation] = useState(location);
-  const [transitionStage, setTransitionStage] = useState("enter");
-
-  useEffect(() => {
-    if (location !== displayLocation) {
-      // Start exit transition
-      setTransitionStage("exit");
-
-      // After exit completes, update location and enter
-      const timer = setTimeout(() => {
-        setDisplayLocation(location);
-        setTransitionStage("enter");
-      }, 200); // Match CSS transition duration
-
-      return () => clearTimeout(timer);
-    }
-  }, [location, displayLocation]);
 
   return (
     <Fragment>
       <Nav />
 
       <main class={`mx-auto max-w-container`}>
-        <Switch>
-          <Route path="/counter" component={Counter} />
-          <Route path="/design" component={DesignPreview} />
-          <Route path="/" component={Index} />
+        <Router>
+          <div className="route-container" key={location}>
+            <Switch>
+              <Route path="/counter" component={Counter} />
+              <Route path="/design" component={DesignPreview} />
+              <Route path="/" component={Index} />
 
-          <Route path="*">No such page!</Route>
-        </Switch>
+              <Route path="*">No such page!</Route>
+            </Switch>
+          </div>
+        </Router>
       </main>
     </Fragment>
   );
